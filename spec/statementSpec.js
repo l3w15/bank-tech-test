168995efab72ab90414fc02b8e87cad0950a2764
Lewis Watson <lewwwiswatson@gmail.com>
2018-03-26 13:53:34 +0100

    :white_check_mark: pass tests for statement.update

--- spec/statementSpec.js
+++ spec/statementSpec.js
@@ -7,4 +7,16 @@ describe('Statement', function() {
   it('is created with a creditsAndDebits Array', function() {
     expect(statement.creditsAndDebits).toEqual([]);
   });
+
+  describe('the .update function', function() {
+    it('updates credits and debits', function() {
+      statement.update(01022018, 500, 'credit', 2000);
+      expect(statement.creditsAndDebits).toEqual([{
+        date: 01022018,
+        amount: 500,
+        type: 'credit',
+        balance: 2000
+      }])
+    })
+  });
 });
--- src/statement.js
+++ src/statement.js
@@ -1,3 +1,13 @@
 function Statement() {
   this.creditsAndDebits = [];
+};
+
+Statement.prototype = {
+  update: function(date, amount, type, balance) {
+    this.creditsAndDebits.push({
+      date: date,
+      amount: amount,
+      type: type,
+      balance: balance });
+  }
 }
